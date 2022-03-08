ffe964a6faa92e107b28510e864b774a86643a7b
Kyler Best <kyler.b@live.com>
2022-03-07 19:34:25 -0600

    commit tree d75f9bf38fe53b5bdb09c7e13d690f4ad6a24eab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,61 @@
 class Dog
+    attr_accessor :name, :breed, :id
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
 
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        DB[:conn].execute("DROP TABLE dogs")
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+
+        self.id = DB[:conn].execute("SELECT id FROM dogs WHERE name = ?", self.name)[0][0]
+
+        self
+    end
+
+    def self.create(name:, breed:)
+        self.new(name: name, breed: breed).save
+    end
+
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.all 
+        DB[:conn].execute("SELECT * FROM dogs").map do |row|
+            self.new_from_db(row)
+        end
+    end
+
+    def self.find_by_name(name)
+        DB[:conn].execute("SELECT * FROM dogs WHERE name = ? LIMIT 1", name).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
+
+    def self.find(id)
+        DB[:conn].execute("SELECT * FROM dogs WHERE id = ? LIMIT 1", id).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
 end
